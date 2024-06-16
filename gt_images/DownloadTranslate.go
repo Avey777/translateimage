7d3c4c405fcb17c7d2ed063bfd4c86a280cb7abb
Jengro Woo <Jengro777@outlook.com>
2024-06-16 22:57:10 +0800

    0616

--- gt_images/DownloadTranslate.go
+++ gt_images/DownloadTranslate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/png"
 	"io"
 	"log"
@@ -9,14 +8,14 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"sync"
 
 	languagecodes "github.com/spywiree/langcodes"
 	"github.com/spywiree/translateimage"
+	// "example.com/google_translateimages/translateimage"
 )
 
-func TranslateImages(imageURLs []string, downloadDir, outputDir string, sourceLaguage, targetLanguage languagecodes.LanguageCode) ([]string, error) {
-
-	var translateFilenames []string
+func TranslateImages(imageURLs []string, downloadDir, outputDir string, sourceLaguage, targetLanguage languagecodes.LanguageCode) (map[string]string, error) {
 
 	// 调用函数下载图片
 	downloadedFilenames, err := DownloadImagesFromURLs(imageURLs, downloadDir)
@@ -24,22 +23,31 @@ func TranslateImages(imageURLs []string, downloadDir, outputDir string, sourceLa
 		log.Print("downloadedFilenames err", err)
 	}
 	// 读取下载的图片，并调用TranslateAndSaveImage函数，将翻译后的图片写入到outputDir目录中
-	for i, dfilename := range downloadedFilenames {
-		outputFilename := "image" + strconv.Itoa(i) + ".png"
-		err := TranslateAndSaveImage(downloadDir, dfilename, outputDir, outputFilename, sourceLaguage, targetLanguage)
-		if err != nil {
-			log.Print("TranslateAndSaveImage err", err)
-			return nil, fmt.Errorf("TranslateAndSaveImage err: %w", err)
-		}
-		translateFilenames = append(translateFilenames, outputFilename)
+	var wg sync.WaitGroup
+
+	for dfilename := range downloadedFilenames {
+		wg.Add(1)
+		go func() {
+
+			err := TranslateAndSaveImage(downloadDir, downloadedFilenames[dfilename], outputDir, downloadedFilenames[dfilename], sourceLaguage, targetLanguage)
+			if err != nil {
+				log.Print("TranslateAndSaveImage err", err)
+				// return nil, fmt.Errorf("TranslateAndSaveImage err: %w", err)
+			}
+
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 	log.Printf("Translate images output Sucessfull")
-	return translateFilenames, nil
+	return downloadedFilenames, nil
 }
 
 // DownloadImagesFromURLs 从URL数组下载图片，并返回下载的图片名称数组
-func DownloadImagesFromURLs(imageURLs []string, downloadDir string) ([]string, []error) {
-	var downloadedFilenames []string // 用于存储下载的图片名称的数组
+func DownloadImagesFromURLs(imageURLs []string, downloadDir string) (map[string]string, []error) {
+	// var downloadedFilenames []string // 用于存储下载的图片名称的数组
+	var translateFilenamesMap map[string]string = make(map[string]string)
+
 	var errors []error
 
 	for i, imageURL := range imageURLs {
@@ -50,10 +58,11 @@ func DownloadImagesFromURLs(imageURLs []string, downloadDir string) ([]string, [
 		DownloadImageFromURL(imageURL, downloadDir, downloadFilename)
 
 		// 将下载的图片文件名添加到数组中
-		downloadedFilenames = append(downloadedFilenames, downloadFilename)
+		// downloadedFilenames = append(downloadedFilenames, downloadFilename)
+		translateFilenamesMap[imageURL] = downloadFilename
 
 	}
-	return downloadedFilenames, errors // 返回下载的图片名称数组
+	return translateFilenamesMap, errors // 返回下载的图片名称数组
 }
 
 // DownloadImageFromURL 下载图片并保存到指定目录
@@ -63,14 +72,14 @@ func DownloadImageFromURL(imageURL, downloadDir, outputFilename string) {
 	// Send an HTTP GET request to retrieve image data
 	response, err := http.Get(imageURL)
 	if err != nil {
-		log.Print("Error downloading image: %v", err)
+		log.Printf("Error downloading image: %v", err)
 		// return
 
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		log.Print("Error downloading image: HTTP status %d", response.StatusCode)
+		log.Printf("Error downloading image: HTTP status %d", response.StatusCode)
 		// return
 	}
 
@@ -82,7 +91,7 @@ func DownloadImageFromURL(imageURL, downloadDir, outputFilename string) {
 	// Create the output directory if it doesn't exist
 	err = os.MkdirAll(filepath.Dir(downloaPath), 0755)
 	if err != nil {
-		log.Print("Error creating output directory: %v", err)
+		log.Printf("Error creating output directory: %v", err)
 		// return
 	}
 
@@ -90,7 +99,7 @@ func DownloadImageFromURL(imageURL, downloadDir, outputFilename string) {
 	// Create the output file
 	downloaFile, err := os.Create(downloaPath)
 	if err != nil {
-		log.Print("Error creating output image file: %v", err)
+		log.Printf("Error creating output image file: %v", err)
 		// return
 	}
 	defer downloaFile.Close()
@@ -99,7 +108,7 @@ func DownloadImageFromURL(imageURL, downloadDir, outputFilename string) {
 	// Write the image data to the output file
 	_, err = io.Copy(downloaFile, response.Body)
 	if err != nil {
-		log.Print("Error saving image to file: %v", err)
+		log.Printf("Error saving image to file: %v", err)
 
 	}
 
@@ -114,7 +123,7 @@ func TranslateAndSaveImage(downloadDir, downloadFilename, outputDir, outputFilen
 	inputPath := filepath.Join(downloadDir, downloadFilename)
 	inputPathAbs, err := filepath.Abs(inputPath)
 	if err != nil {
-		log.Print("Error getting absolute path for input image: %v", err)
+		log.Printf("Error getting absolute path for input image: %v", err)
 		// return err
 	}
 
@@ -125,7 +134,7 @@ func TranslateAndSaveImage(downloadDir, downloadFilename, outputDir, outputFilen
 		inputPathAbs, sourceLaguage, targetLanguage,
 	)
 	if err != nil {
-		log.Print("Error translating image: %v", err)
+		log.Printf("Error translating image: %v", err)
 		// return err
 	}
 
@@ -137,7 +146,7 @@ func TranslateAndSaveImage(downloadDir, downloadFilename, outputDir, outputFilen
 	// Create the output directory if it doesn't exist
 	err = os.MkdirAll(filepath.Dir(outputPath), 0755)
 	if err != nil {
-		log.Print("Error creating output directory: %v", err)
+		log.Printf("Error creating output directory: %v", err)
 		return err
 	}
 
@@ -145,7 +154,7 @@ func TranslateAndSaveImage(downloadDir, downloadFilename, outputDir, outputFilen
 	// Create the output file
 	f, err := os.Create(outputPath)
 	if err != nil {
-		log.Print("Error creating output image file: %v", err)
+		log.Printf("Error creating output image file: %v", err)
 		return err
 	}
 	defer f.Close()
@@ -154,70 +163,10 @@ func TranslateAndSaveImage(downloadDir, downloadFilename, outputDir, outputFilen
 	// Encode the translated image in PNG format and write it to the file
 	err = png.Encode(f, img)
 	if err != nil {
-		log.Print("Error encoding and saving output image: %v", err)
+		log.Printf("Error encoding and saving output image: %v", err)
 		return err
 	}
 
 	log.Printf("Image translated and saved to %s", outputPath)
 	return nil
 }
-
-// func main() {
-
-// 	inputDir := "inputDir"
-// 	downInputImagesName := "image0" + ".png"
-
-// 	// 调用DownloadImageFromURL函数，传入图片的URL、输出目录和输出文件名
-// 	// Call the DownloadImageFromURL function, passing the image URL, output directory, and output file name as parameters
-// 	imageURL := "https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg"
-// 	DownloadImageFromURL(imageURL, inputDir, downInputImagesName)
-
-// 	//调用TranslateAndSaveImage函数，传入图片所在的目录、文件名、输出目录和输出文件名
-// 	// Call the TranslateAndSaveImage function, passing the image directory, filename, output directory, and output filename as parameters
-// 	TranslateAndSaveImage(inputDir, downInputImagesName, "outputDir/images", "output1.png",languagecodes.DETECT_LANGUAGE, languagecodes.ENGLISH)
-// }
-
-// func main() {
-// 	// 图片URL数组
-// 	imageURLs := []string{
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		// ... 其他URL ...
-// 	}
-
-// 	// 输出目录
-// 	downloadDir := "downloadDir" + "/" + "09343435"
-
-// 	// 调用函数下载图片
-// 	downloadedFilenames := DownloadImagesFromURLs(imageURLs, downloadDir)
-// 	log.Fatal(downloadedFilenames)
-// }
-
-// func main() {
-// 	// 图片URL数组
-// 	imageURLs := []string{
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		"https://cbu01.alicdn.com/img/ibank/O1CN01LdZDpx1qMLl44TLGV_!!2215685915481-0-cib.jpg",
-// 		// ... 其他URL ...
-// 	}
-
-// 	// 输出目录
-// 	downloadDir := "downloadDir" + "/" + "09343435"
-// 	outputDir := "outputDir" + "/" + "09343435"
-
-// 	// 调用函数下载图片
-// 	downloadedFilenames, err := DownloadImagesFromURLs(imageURLs, downloadDir)
-// 	if err != nil {
-// 		log.Fatal("downloadedFilenames err", err)
-// 	}
-
-// 	// 读取下载的图片，并调用TranslateAndSaveImage函数，将翻译后的图片写入到outputDir目录中
-// 	for i, dfilename := range downloadedFilenames {
-// 		outputFilename := "image" + strconv.Itoa(i) + ".png"
-// 		TranslateAndSaveImage(downloadDir, dfilename, outputDir, outputFilename, languagecodes.DETECT_LANGUAGE, languagecodes.ENGLISH)
-
-// 	}
-// 	// log.Fatalf("Sucessfull")
-// }
